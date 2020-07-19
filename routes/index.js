54789e41fea250a3d6aead3cebed6cae7008a304
Aravind <aravind08222@gmail.com>
2020-07-19 13:56:43 +0530

    initiated nodemailer coding for forgot password

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var passport = require("passport");
+var nodemailer = require("nodemailer");
+var smtpTransport = require("nodemailer-smtp-transport");
 var User = require("../models/user");    
 
 //home page of the website
@@ -61,8 +63,45 @@ router.get('/forgot', function (req, res){
 });
 
 router.post('/forgot', function(req, res){
-    req.flash("success", "Click the link on the mail to change password");
-    res.redirect("/campground")
+    async.waterfall([
+        function(done) {
+          crypto.randomBytes(20, function(err, buf) {
+            var token = buf.toString('hex');
+            done(err, token);
+          });
+        },
+        function(token, done) {
+          User.findOne({ email: req.body.email }, function(err, user) {
+            if (!user) {
+              req.flash('error', 'No account with that email address exists.');
+              return res.redirect('/api/forgot');
+            }
+    
+            user.resetPasswordToken = token;
+            user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+    
+            user.save(function(err) {
+              done(err, token, user);
+            });
+          });
+        },
+        function(token, user, done) {
+          var fromEmail = new helper.Email('aravind08222@gmail.com');
+          var toEmail = new helper.Email(user.email);
+          var subject = 'Password Reset from Info Globe';
+          var content = new helper.Content('text/plain','You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+               'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+               'http://' + req.headers.host + '/api/reset/' + token + '\n\n' +
+               'If you did not request this, please ignore this email and your password will remain unchanged.\n');
+          var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+            
+          //Sending mail using gmail
+
+          req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+          res.redirect('/api/forgot');   
+        }]);
+    // req.flash("success", "Click the link on the mail to change password");
+    // res.redirect("/campground")
 });
 
 
