00b15749b8ea0b74baf598e7c8ed925871eea0bb
Aravind <aravind08222@gmail.com>
2020-08-02 13:30:42 +0530

    changes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var campgroundRoutes = require("./routes/campgroud"),
     indexRoutes = require("./routes/index");
 
 //mongoose.connect(process.env.MONGODB, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
-mongoose.connect(process.env.MONGODBCS.toString(), { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
+mongoose.connect(process.env.MONGODBCS, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static('./public/'));
 app.set("view engine", "ejs");
