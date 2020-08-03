1e826f9a4fec20cb438c28010fc731791eb09290
Aravind <aravind08222@gmail.com>
2020-08-03 14:01:53 +0530

    cleaned up comment route

--- routes/comment.js
+++ routes/comment.js
@@ -5,72 +5,10 @@ var Campground = require("../models/campground"),
     middleware = require("../middleware");
 const comment = require("../controllers/comment");
 
-//Comments get Route
-router.get("/new", middleware.isLoggedin, comment.addCommentForm);
-
-//comments post route
-router.post("/", middleware.isLoggedin, function (req, res) {
-    Campground.findById(req.params.id, function (err, campground) {
-        if (err) {
-            console.log(err);
-            req.flash("error", "Something Went Wrong");
-            res.redirect("/campground");
-        } else {
-            Comment.create(req.body.comment, function (err, comment) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    //add username and id to comment
-                    comment.author.id = req.user._id;
-                    comment.author.username = req.user.username;
-                    //save comment
-                    comment.save();
-                    campground.comments.push(comment);
-                    campground.save();              
-                    req.flash("success", "Comment Created Successfully");
-                    res.redirect("/campground/" + campground._id);
-                }
-            });
-        }
-    });
-});
-
-//Comment Edit route
-router.get("/:comment_id/edit", middleware.checkcommentownership, function (req, res) {
-    Comment.findById(req.params.comment_id, function (err, foundcomment) {
-        if (err) {
-            console.log(err);
-            res.redirect("back");
-        } else {            
-            res.render("cmntsedit", { campground_id: req.params.id, comment: foundcomment });
-        }
-    });    
-});
-
-//Comment Update Route
-router.put("/:comment_id", middleware.checkcommentownership, function (req, res) {
-    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedcomment) {
-        if (err) {
-            console.log(err);
-            res.redirect("back");
-        } else {
-            req.flash("success", "Comment Edited Succesfully");            
-            res.redirect("/campground/" + req.params.id);
-        }
-    });
-});
-
-//Comment destroy route
-router.delete("/:comment_id", middleware.checkcommentownership, function (req, res) {
-    Comment.findByIdAndRemove(req.params.comment_id, function (err, deleted, comment) {
-        if (err) {
-            console.log(err);
-            res.redirect("back");
-        } else {
-            req.flash("success", "Comment Deleted Succesfully");            
-            res.redirect("/campground/" + req.params.id);
-        }
-    })
-});
+router.get("/new", middleware.isLoggedin, comment.newCommentForm);
+router.post("/", middleware.isLoggedin, comment.addComment);
+router.get("/:comment_id/edit", middleware.checkcommentownership, comment.editComment);
+router.put("/:comment_id", middleware.checkcommentownership, comment.updateComment);
+router.delete("/:comment_id", middleware.checkcommentownership, comment.deleteComment);
 
 module.exports = router;
