e18371c11dea525850fb6162355c7db2df5e88d1
Aravind <aravind08222@gmail.com>
2020-08-14 13:47:49 +0530

    FIle upload code tranfered from book routes to book controllers

--- controllers/book.js
+++ controllers/book.js
@@ -3,6 +3,14 @@ const viewBalancerHelper = require("../helpers/viewbalancer");
 const updateTimeHelper = require("../helpers/updateTime");
 const createTimeHelper = require("../helpers/createTime");
 
+var cloudinary = require("cloudinary");
+cloudinary.config({ 
+    cloud_name: 'dbbinc37j', 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  });
+
+
 function escapeRegex(text) {
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 };
@@ -34,7 +42,40 @@ exports.indexPage = function(req, res){
 
 //Post req submission for new Book
 exports.createBookPostreq = async function(req,res){
-    
+    console.log("----------------");
+    await cloudinary.uploader.upload(req.file.path, function(result){
+        console.log(result);
+        req.body.image = result.secure_url;
+    })
+    console.log("------------------------------");
+    console.log(req.body)
+    var name = req.body.name;
+    var image = req.body.image;
+    var price = req.body.price;
+    var description = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var date = createTimeHelper.createTime();
+    var newbook = { 
+        name: name, 
+        image: image, 
+        description: description, 
+        author: author, 
+        price: price,
+        createdAt: date
+    };
+    Book.create(newbook, function (err, newbook) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(">--------------------------------------------------------New book Created");
+            console.log(newbook);
+            req.flash("success", "Book Created Successfully");
+            res.redirect("/books/" + newbook._id);
+        }
+    });
 }
 
 //Form for new Book
--- routes/book.js
+++ routes/book.js
@@ -31,42 +31,7 @@ cloudinary.config({
   });
 
 router.get("/",book.indexPage);
-router.post("/", upload.single('image') ,async function(req, res){
-    console.log("----------------");
-    await cloudinary.uploader.upload(req.file.path, function(result){
-        console.log(result);
-        req.body.image = result.secure_url;
-    })
-    console.log("------------------------------");
-    console.log(req.body)
-    var name = req.body.name;
-    var image = req.body.image;
-    var price = req.body.price;
-    var description = req.body.description;
-    var author = {
-        id: req.user._id,
-        username: req.user.username
-    };
-    var date = createTimeHelper.createTime();
-    var newbook = { 
-        name: name, 
-        image: image, 
-        description: description, 
-        author: author, 
-        price: price,
-        createdAt: date
-    };
-    Book.create(newbook, function (err, newbook) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(">--------------------------------------------------------New book Created");
-            console.log(newbook);
-            req.flash("success", "Book Created Successfully");
-            res.redirect("/books/" + newbook._id);
-        }
-    });
-});
+router.post("/", upload.single('image'), book.createBookPostreq);
 router.get("/new", book.createBookForm);
 router.get("/:id", book.viewBook); 
 router.get("/:id/edit", middleware.checkbookownership, book.editBook);
