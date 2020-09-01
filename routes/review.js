151da87fccefe82319d45e80a673a00fc366d1cf
Aravind <aravind08222@gmail.com>
2020-09-01 20:06:52 +0530

    Completed reviews index in routes and campground

--- routes/review.js
+++ routes/review.js
@@ -3,3 +3,6 @@ const router = express().Router({ mergeParams: true });
 const review = require("../controllers/review");
 const middleware = require("../middleware/Index");
 
+router.get("/", review.reviewIndex);
+
+module.exports = router;
