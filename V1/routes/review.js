67f89ab0cf064a19626d05827398f4450a8f244b
Aravind <aravind08222@gmail.com>
2020-11-11 20:46:28 +0530

    m

--- V1/routes/review.js
+++ V1/routes/review.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router({mergeParams: true});
 const review = require("../controllers/review");
-const middleware = require("../middleware/Index.js");
+const middleware = require("../middleware/index.js");
 
 router.get("/", review.reviewIndex);
 router.get("/new", middleware.isLoggedin, middleware.checkReviewExistence, review.reviewNew);
